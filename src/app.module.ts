9c82b8a1473e0654734107597c243625469c95cb
Amr <amrhammoud.dev@gmail.com>
2024-01-10 13:46:50 +0200

    Created Patient Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DoctorModule } from './doctor/doctor.module';
+import { PatientModule } from './patient/patient.module';
 
 @Module({
-  imports: [DoctorModule],
+  imports: [DoctorModule, PatientModule],
   controllers: [AppController],
   providers: [AppService],
 })
