847ceb113ef01ca7a653aea5eebb58b6f840d5a5
Amr <amrhammoud.dev@gmail.com>
2024-01-18 11:38:44 +0200

    Added Guard to delete endpoint

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -43,6 +43,7 @@ export class UserController {
     return this.userService.update(+id, updateUserDto);
   }
 
+  @UseGuards(new RoleGuard(['ADMIN']))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.userService.remove(+id);
