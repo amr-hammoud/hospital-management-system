ca0a2111be04e8a51f2aca3029b6c2ea72e2c19f
Amr <amrhammoud.dev@gmail.com>
2024-01-18 12:36:34 +0200

    Created Appointment Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { DoctorModule } from './doctor/doctor.module';
+import { AppointmentModule } from './appointment/appointment.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, AuthModule, DoctorModule]
+  imports: [UserModule, AuthModule, DoctorModule, AppointmentModule]
 })
 export class AppModule {}
